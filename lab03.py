ce844a35f2835df965a74f4f6ae92672c745b8b0
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-06-25 08:00:52 +0530

    Create lab03.py
    first part

---
+++ lab03.py
@@ -0,0 +1,28 @@
+#providing main function
+def main() :
+    student_details = {
+        'student_name' : 'Pujan Patel',
+
+        'student_id' : '10266348',
+
+        'pizza_toppings' :
+        [
+            'pineapple',
+            'Black olives',
+            'pepper',
+        ],
+        'movies' :
+        [
+            {
+                'title' : 'The Hangover',
+                'genre' : 'Comedy'
+            },
+            {
+                'title' : 'Bubble'
+                'genre' : [
+                'anime',
+                'parkour',
+                'adventure']
+            }
+        ]
+    }
