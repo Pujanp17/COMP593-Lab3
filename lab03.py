ead1cc60a13ee39988d0820cdc5fa8ca7dc7563e
Pujanp17 <105946631+Pujanp17@users.noreply.github.com>
2022-06-25 08:05:00 +0530

    Update lab03.py

--- lab03.py
+++ lab03.py
@@ -26,3 +26,59 @@ def main() :
             }
         ]
     }
+
+    # adding new movie
+    new_movie = {'title' : 'Kings man', 'genre' : ['Adventure', 'Climax']}
+    student_details['movies'].append(new_movie)
+
+    # adding  topping
+    new_topping = ['pizza seasoning']
+    student_details['pizza_toppings'].append(new_topping)
+
+#defining add to and modify each element in a list
+def adding_topping(details, new_topping) :
+
+    #adding topping
+    details['topping'].extend(new_topping)
+
+    #letter in lowercase
+    for i,p in enumerate(details['topping']) :
+        details['topping'][i] = p.lower()
+
+    #sorting the toppings in alphabetical order
+    details['topping'].sort()
+
+#printing a sentence using data from main
+def print_sentence(details) :
+    sentence = f"My name is" + {details['student_name']} + "but you can call me Sir" + {details['student_name'].removesuffix('Singh')} + '.\n'
+    "My student ID is" + {details['student_id']}
+    print(sentence, "end = '\n\n'")
+
+#printing the toppings with bullets
+def bullets_topping(details) :
+    print("My favourite toppings are:")
+
+    for p in details['topping']:
+        print(f"- {p}")
+    print()
+
+#printing genres
+def comma_genre(details) :
+    print(f"My goto genres are", end='')
+    for i,g in enumerate(details['movies']) :
+        print(g['genre'], end='')
+        if i < len(details['movies'])-1:
+            print(', ', end='')
+    print('.', end='\n\n')
+
+#printing movies
+def comma_gen(details) :
+    print(f"My favourite movies are", end='')
+    for i,g in enumerate(details['movies']) :
+        print(g['title'], end='')
+        if i < len(details['movies'])-1:
+            print(', ', end='')
+    print('.', end='\n\n')
+
+#calling main function
+main()
